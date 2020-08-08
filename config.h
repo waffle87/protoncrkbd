86569ae3529c8a0945b1fe439f93b3deafea88b1
jsangdahl <59737601+jsangdahl1@users.noreply.github.com>
2020-08-08 01:08:24 -0600

    Upload Initial Files

---
+++ config.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#undef USE_I2C
+#undef SSD1306OLED
+
+#define MASTER_LEFT
+
+#define USE_SERIAL_PD2
+
+#define UNICODE_SELECTED_MODES UC_WINC, UC_MAC, UC_LNX
+
+#define UNICODE_CYCLE_PERSIST false
+
+#define AUTO_SHIFT_MODIFIERS
+#define AUTO_SHIFT_TIMEOUT 150
+#define NO_AUTO_SHIFT_SPECIAL
+#define NO_AUTO_SHIFT_NUMERIC
+
+#ifdef LOCKING_SUPPORT_ENABLE
+#    undef LOCKING_SUPPORT_ENABLE
+#endif
+#ifdef LOCKING_RESYNC_ENABLE
+#    undef LOCKING_RESYNC_ENABLE
+#endif
+
+#define IGNORE_MOD_TAP_INTERRUPT
+#define PERMISSIVE_HOLD
+#define TAPPING_TERM 210
+
+#define NO_ACTION_ONESHOT
+
+#define OLED_FONT_H "keyboards/crkbd/keymaps/sixcol/glcdfont.c"
