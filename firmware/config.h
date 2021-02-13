87ae7d171dc0ce9a0fe54edeb3dd5cb8995b6cd8
Jack <applej955@gmail.com>
2021-02-12 17:00:45 -0700

    Add fully working config

--- firmware/config.h
+++ firmware/config.h
@@ -87,11 +87,11 @@
 #endif
 
 //RGB using PWM on pin B0
-#define WS2812_PWM_DRIVER PWMD2  // default: PWMD2
-#define WS2812_PWM_CHANNEL 2  // default: 2
+#define WS2812_PWM_DRIVER PWMD3  // default: PWMD2
+#define WS2812_PWM_CHANNEL 3  // default: 2
 #define WS2812_PWM_PAL_MODE 2  // Pin "alternate function" - default: 2
-#define WS2812_DMA_STREAM STM32_DMA1_STREAM2  // DMA Stream for TIMx_UP
-#define WS2812_DMA_CHANNEL 2  // DMA Channel for TIMx_UP
+#define WS2812_DMA_STREAM STM32_DMA1_STREAM3  // DMA Stream for TIMx_UP
+#define WS2812_DMA_CHANNEL 3  // DMA Channel for TIMx_UP
 
 //Serial over USART config
 #undef SOFT_SERIAL_PIN
